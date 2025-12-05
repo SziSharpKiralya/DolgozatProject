3f68e83d08ee628a6de98c5ef702c488f89ed690
Petrik Diak <diak@petrik.hu>
2025-12-05 13:18:07 +0100

    Első feladat határértékek tesztelve

--- DolgozatProject/Dolgozat.cs
+++ DolgozatProject/Dolgozat.cs
@@ -8,5 +8,21 @@
 		{
 			this.pontok = [];
 		}
+
+		public void PontFelvesz(int pont)
+		{
+			if (pont < -1)
+			{
+				throw new ArgumentException("Ez nem lehet megadott pontszám!");
+			}
+			else if (pont > 100)
+			{
+				throw new ArgumentException("Ez meghaladja a maximális megadott pontszámot!");
+			}
+			else
+			{
+				this.pontok.Add(pont);
+			}
+		}
 	}
 }
--- DolgozatProject/Tests/DolgozatTest.cs
+++ DolgozatProject/Tests/DolgozatTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace DolgozatProject.Tests
 {
+	[TestFixture]
 	internal class DolgozatTest
 	{
+		Dolgozat dolgozat;
+
+		[SetUp]
+		public void SetUp()
+		{
+			dolgozat = new Dolgozat();
+		}
+
+		[Test]
+		public void LehetetlenPontszam01()
+		{
+			Assert.Throws<ArgumentException>(
+				() =>
+				{
+					dolgozat.PontFelvesz(-2);
+				}
+			);
+		}
+
+		[Test]
+		public void LehetetlenPontszam02()
+		{
+			Assert.Throws<ArgumentException>(
+				() =>
+				{
+					dolgozat.PontFelvesz(101);
+				}
+			);
+		}
 	}
 }
